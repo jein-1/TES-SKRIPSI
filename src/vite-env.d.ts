ea82ceee5de8aad06c70338b15ed0ef5fd5fb3d6
Mahasiswa Skripsi <skripsi@student.ac.id>
2026-04-22 07:41:45 +0800

    update peta versi 17

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_ADMIN_APP: string
+  readonly VITE_ADMIN_APP: string;
 }
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
