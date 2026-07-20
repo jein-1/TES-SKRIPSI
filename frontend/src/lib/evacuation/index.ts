463ac39ecc8d6528324c96cf9a62cd0a4bcd7754
Mahasiswa Skripsi <skripsi@student.ac.id>
2026-07-20 14:25:52 +0800

    fix: export addCustomShelter in evacuation index

--- frontend/src/lib/evacuation/index.ts
+++ frontend/src/lib/evacuation/index.ts
@@ -7,7 +7,7 @@
  */
 
 export type { Point, Shelter, RoadNode, RoadEdge, RouteResult, DijkstraResult } from './types.ts'
-export { shelters }              from './shelters.ts'
+export { shelters, addCustomShelter } from './shelters.ts'
 export { hazardZones }           from './hazardZones.ts'
 export { roadNodes, roadEdges }  from './roadNetwork.ts'
 export { calculateHaversine, findNearestNode } from './haversine.ts'
